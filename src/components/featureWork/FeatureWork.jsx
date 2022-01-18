65592b6e13025b49a9c6167a3a42673f14ff1a72
onitiucosmin <48385722+onitiucosmin@users.noreply.github.com>
2022-01-18 07:45:39 +0100

    Updated mobile responsiveness

--- src/components/featureWork/FeatureWork.jsx
+++ src/components/featureWork/FeatureWork.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 
 const FeatureWork = ({ title, period, list }) => (
   <div className="gpt3__features-container__feature-tech row">
-    <div className="gpt3__features-container__feature-title-tech col-5">
-      <div />
-      <h1>{title}</h1>
-      <p>{period}</p>
+    <div className="col-lg-5 col-md-12">
+      <div className="gpt3__features-container__feature-title-tech">
+        <div />
+        <h1>{title}</h1>
+        <p>{period}</p>
+      </div>
     </div>
-    <div className="gpt3__features-container_feature-text-tech col-7">
-      <ul>
-        {list.map((item) => (
-          <li>{item}</li>
-        ))}
-      </ul>
+    <div className="col-lg-7 col-md-12">
+      <div className="gpt3__features-container_feature-text-tech">
+        <ul>
+          {list.map((item) => (
+            <li>{item}</li>
+          ))}
+        </ul>
+      </div>
     </div>
     <hr style={{ color: 'white' }} />
   </div>
